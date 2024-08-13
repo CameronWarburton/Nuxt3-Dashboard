14c1d5475840a494a97d6643a7ef12978d36f16e
Cameron <cameron.cw@outlook.com>
2024-08-13 13:10:29 +0100

    Created a custom snippet to save time for each page

--- pages/transactions.vue
+++ pages/transactions.vue
@@ -1,7 +1,14 @@
 <script setup>
-const loading = ref(false)
+const loading = ref(false);
 </script>
 
 <template>
-    <div>hello transactions</div>
+  <div>
+    <header>This is my header</header>
+    <main>
+      This is my main
+      <section>This is my section</section>
+    </main>
+    <footer>This is my footer</footer>
+  </div>
 </template>
