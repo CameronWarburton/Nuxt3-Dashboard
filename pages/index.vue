fa398f547cecd4ae9fc59f5190e51498bc17acb5
Cameron <cameron.cw@outlook.com>
2024-08-19 21:00:06 +0100

    Created skeleton of UI

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,26 @@ const loading = ref(false);
 </script>
 
 <template>
-  <div class="grid gap-8 h-screen">
-    <header>
-      <p>Hi, Welcome back Cameron!</p>
-      <h1>Dashboard</h1>
+  <div class="grid w-full gap-4 h-screen">
+    <header class="flex items-start justify-between">
+      <div class="grow">
+        <p>Hi, Welcome back Cameron!</p>
+        <h1>Dashboard</h1>
+      </div>
+      <div class="bg-neutral-200 h-[36px] w-[120px]"></div>
     </header>
-    <main>
-      <div>Tabs: Today This week This month</div>
-      <section>Chart</section>
+    <main class="grid gap-2">
+      <div class="flex items-center gap-3">
+      <div v-for='(item, index) in 3' :key='index' class="w-[120px] h-[36px] bg-neutral-200"></div>
+      </div>
+      <section>
+      <div class="w-full h-[360px] bg-neutral-200"></div>
+      </section>
     </main>
-    <footer>I'll have 3 different cards</footer>
+    <footer>
+    <div class="flex items-center gap-4">
+    <div v-for='(item, index) in 3' :key='index' class="w-full h-[260px] bg-neutral-200"></div>
+    </div>
+    </footer>
   </div>
 </template>
