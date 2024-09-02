2efc093dc8d9c60f284c7bc210aefa5ac60d8b54
Cameron <cameron.cw@outlook.com>
2024-09-02 17:25:12 +0100

    Made a start on card component

---
+++ components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+const props = defineProps(["card"]);
+</script>
+
+<template>
+    <Card>
+    <CardHeader></CardHeader>
+    </Card>
+</template>
