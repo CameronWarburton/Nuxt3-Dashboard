35e839c2417ff8b2e398a27378955ab26222da8c
Cameron <cameron.cw@outlook.com>
2024-09-04 13:08:18 +0100

    Added Dialog Box for adding a new product

--- components/Product/New.vue
+++ components/Product/New.vue
@@ -11,29 +11,32 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
-const loading = ref(false);
+const { loading, open } = useHelpers(); // true
 
 const createNewProduct = async () => {
   try {
     loading.value = true;
-    // all the business logic 
+    // all the business logic
   } catch (error) {
-
   } finally {
     loading.value = false;
+    open.value = false;
   }
 };
 </script>
 
 <template>
-  <AlertDialog>
+  <AlertDialog :open="open">
     <AlertDialogTrigger>
-      <Button>Add a new product</Button>
+      <Button @click="open = true">Add a new product</Button>
     </AlertDialogTrigger>
     <AlertDialogContent>
-      <AlertDialogHeader> </AlertDialogHeader>
+      <AlertDialogHeader> 
+      <AlertDialogTitle>Create a new product</AlertDialogTitle>
+      <Input placeholder="My product name" />
+      </AlertDialogHeader>
       <AlertDialogFooter>
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogCancel @click="open = false">Cancel</AlertDialogCancel>
         <AlertDialogAction @click="createNewProduct()"
           >Create</AlertDialogAction
         >
