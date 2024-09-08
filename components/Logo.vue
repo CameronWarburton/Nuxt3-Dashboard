2f8337c88d4b1e5a682bcadc1218837760bcacfb
Cameron <cameron.cw@outlook.com>
2024-09-08 16:48:55 +0100

    Edited Logo Component again

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="overflow-hidden rounded-lg w-[45px]">
-    <img src="../assets/logo.png" alt="Logo" />
+    <img src="@/assets/logo.png" alt="Logo" />
   </div>
 </template>
